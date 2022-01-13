332e7e9817844ee5e882a24ba1c29e80fd02d3ac
jacobboim <jacob.boim30@gmail.com>
2022-01-12 20:50:43 -0500

    trying again

---
+++ build/precache-manifest.71cf2d710f5c18119d79eb28a3eae781.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fe24423ae6568e914bfed3a0d435881",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87bbd5fbd4a10c4117d2",
+    "url": "/static/css/main.84f21122.chunk.css"
+  },
+  {
+    "revision": "1fe16370e8c4f38c82fb",
+    "url": "/static/js/2.dd78ace4.chunk.js"
+  },
+  {
+    "revision": "4ed2d4f00cf8324f872f01907affe619",
+    "url": "/static/js/2.dd78ace4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87bbd5fbd4a10c4117d2",
+    "url": "/static/js/main.3eeeb647.chunk.js"
+  },
+  {
+    "revision": "b21e2db45bb5cd26f15a",
+    "url": "/static/js/runtime-main.dca7fc49.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
